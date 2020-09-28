713e4f9a282e2acba6b869943e2f8933ceef15c2
Austin Trask <aj45trask@gmail.com>
2020-09-28 17:33:24 -0500

    Added tracking script with OLED display functionality plus some other changes

--- Tracking with Display.py
+++ Tracking with Display.py
@@ -1,4 +1,4 @@
-# HABET Customer Payload CircuitPython Script
+# HABET Basic Tracking Payload CircuitPython Script
 # Recieves GPS data and transmits it via LoRa radio module
 # Utilizes Adafruit Feather M4 Express, Ultimate GPS FeatherWing, RFM95W 433 MHz FeatherWing
 # Last updated 9/2/2020 by Austin Trask
@@ -9,6 +9,7 @@ import busio
 import digitalio
 import analogio
 import adafruit_rfm9x
+import adafruit_ssd1306
 
 # Device ID
 FEATHER_ID = b'1'
@@ -43,27 +44,19 @@ TX = board.TX
 # Set the UART serial connection for the GPS module
 uart = busio.UART(TX, RX, baudrate=9600, timeout=1)
 
-# Payload 1 UART
-uartPayload1= busio.UART(board.A2, board.A3, baudrate=9600, timeout=30)
-
-# Payload 2 UART
-uartPayload2 = busio.UART(board.A4, board.A5, baudrate=9600, timeout=30)
-
 # Define the send message function
-def sendGPS(message):
+def sendMessage(message, packet_time):
     try:
         # Measures battery voltage and formats it to transmit
         battery_voltage = get_voltage(vbat_voltage)
         v = "{:.2f}".format(battery_voltage)
-        rfm9x.send(FEATHER_ID+b','+message+',v,'+v)
-    except:
-        print("GPS failed to send")
-
-def sendData(message):
-    try:
-        rfm9x.send(FEATHER_ID+b','+message)
+        rfm9x.send(FEATHER_ID+b','+message+'V,'+v)
+        packet_time = time.monotonic()
+        print("Packet Sent")
     except:
         print("Message failed to send")
+        packet_time = packet_time
+    return packet_time
 
 # Define the get voltage function
 def get_voltage(pin):
@@ -73,9 +66,19 @@ current = time.monotonic()
 old = current
 newGPS = False
 
+i2c = busio.I2C(board.SCL, board.SDA)
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+oled.fill(0)
+oled.text('Hello', 0, 0, 1)
+#oled.text('World', 0, 10, 1)
+oled.show()
+
+new_gps_time = 0
+last_packet_sent = 0
+
 while True:
     current = time.monotonic()
-    
+
     # Checks for new GPS data
     if uart.in_waiting > 0:
         gps_string = uart.readline()
@@ -83,50 +86,23 @@ while True:
         if "GPGGA" in gps_string:
             gps_str = str(gps_string)
             newGPS = True
-            
+            new_gps_time = current
+
     # Sends GPS data every 5 seconds (or 5 times whatever your later sleep value is)
     if current-old>5:
         old = current
         if newGPS:
             print(gps_str)
-            sendGPS(gps_str)
+            last_packet_sent = sendMessage(gps_str, last_packet_sent)
             newGPS = False
         else:
             # Message for if there is no GPS
-            sendGPS("No GPS")
+            sendMessage("No GPS")
             print("No GPS")
 
-    #Gets data from payload #1
-    data1 = None
-    uartPayload1.write("Ready")
-    reccount = 0
-    while data1 is None and reccount < 10:
-        data1 = uartPayload1.read(241)
-        sendstr1 = 'P1Dat,'+''.join([chr(b) for b in data1])
-        time.sleep(.5)
-        reccount = reccount + 1
-    if data1 is None:
-        sendData('No Payload 1 Data')
-        print('No Payload 1 Data')
-    else:
-        sendData(sendstr1)
-        print(sendstr1)
-
-    #Gets data from payload #2
-    data2 = None
-    uartPayload2.write("Ready")
-    reccount = 0
-    while data2 is None and reccount < 10:
-        data2 = uartPayload1.read(241)
-        sendstr2 = 'P2Dat,'+''.join([chr(b) for b in data1])
-        time.sleep(.5)
-        reccount = reccount + 1
-    if data2 is None:
-        sendData('No Payload 2 Data')
-        print('No Payload 2 Data')
-    else:
-        sendData(sendstr2)
-        print(sendstr2)
-            
     # Pauses for 1 second before restaring the cycle
+    oled.fill(0)  # this will wipe the display clean
+    oled.text("New GPS t+ "+str(int(current-new_gps_time)) , 0, 8, 1)
+    oled.text("Packet Sent t+ "+str(int(current-last_packet_sent)) , 0, 16, 1)
+    oled.show()
     time.sleep(.1)
